994dcf4e357267692455512dea5665bc17b42b14
Meet Shihora <142139362+meetshihora@users.noreply.github.com>
2024-10-29 09:44:36 +0530

    Add files via upload

---
+++ scanf.c.c
@@ -0,0 +1,13 @@
+main()
+{
+    int a,b,c;
+    printf("enter the eny number:");
+    scanf("%d", &a);
+
+    printf("enter the second number:");
+    scanf("%d",&b);
+
+    c = a+b;
+    printf("your total answer is %i", (c));
+getch();
+}
